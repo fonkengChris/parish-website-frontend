d296a8c908496a1439fb81234a1b937ae8d9d809
fonkengChris <echristianfonkeng@gmail.com>
2025-11-29 21:18:16 +0000

    Fix ts issues

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import LazyImage from '../components/LazyImage';
 import { eventsAPI } from '../services/api';
